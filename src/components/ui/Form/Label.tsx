bf95052cff62b68f055c746d85541df1b02e6a69
Elton Pongilo <elton.pongilo@gmail.com>
2023-12-05 12:19:47 -0300

    Recognize and Form components

--- src/components/ui/Form/Label.tsx
+++ src/components/ui/Form/Label.tsx
@@ -1,3 +1,12 @@
-export function Label({ children }: { children: string }) {
-  return <p className="text-title-2 text-white">{children}</p>
+import { ComponentProps } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+type LabelProps = ComponentProps<'label'>
+
+export function Label({ className, children, ...restProps }: LabelProps) {
+  return (
+    <label {...restProps} className={twMerge('block text-title-2 text-white', className)}>
+      {children}
+    </label>
+  )
 }
