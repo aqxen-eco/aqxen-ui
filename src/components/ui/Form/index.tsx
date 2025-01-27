3e431f89acfb35f53c2499f7f62c3bb07fd344e3
Bruno Souza <brnosouza@gmail.com>
2025-01-27 15:01:24 -0300

    Sunsetting old upscale

--- src/components/ui/Form/index.tsx
+++
@@ -1,4 +0,0 @@
-export { Field } from '@/components/ui/Form/Field'
-export { Input } from '@/components/ui/Form/Input'
-export { Label } from '@/components/ui/Form/Label'
-export { TextArea } from '@/components/ui/Form/TextArea'
--- src/styles/input.ts
+++
@@ -1,5 +0,0 @@
-import { tv } from 'tailwind-variants'
-
-export const input = tv({
-  base: 'body-2 block w-full bg-transparent py-2 placeholder:text-gray-3 focus:outline-none'
-})
